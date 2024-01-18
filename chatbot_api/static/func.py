90f013e49d45745545d05eb82d172add4354650b
t4ngentt <rohanshegokar@gmail.com>
2024-01-18 12:14:18 +0530

    added chatbot memory buffer

--- chatbot_api/static/func.py
+++ chatbot_api/static/func.py
@@ -3,6 +3,8 @@ import os
 from langchain.chains import LLMChain, SimpleSequentialChain
 from langchain.prompts import PromptTemplate
 from langchain_openai import OpenAI
+from langchain.memory import ConversationBufferMemory
+
 
 import yaml
 
@@ -13,15 +15,26 @@ with open('./config.yaml', 'r') as file:
 OPENAI_API_KEY = getpass()
 os.environ["OPENAI_API_KEY"] = keydic['API_KEY']
 
-def initaite_query(position, query):
+memory = ConversationBufferMemory(memory_key="chat_history", input_key="human_input")
+
+def initaite_query(position, human_input):
+
     sllm = OpenAI(temperature=0.5)
     
     main_prompt = PromptTemplate(
-    input_variables=['position','query'],
-    template="Answer the following question as a seasoned {position}. Answer in human language.: {query}"
+    input_variables=['position', 'human_input', 'chat_history'],
+    template="""You are a as a seasoned {position}. You are chatting with a user.
+    
+    Chat History:   
+
+    {chat_history}
+    User: {human_input}
+    You (AI): 
+    """
     )
 
-    chain = LLMChain(llm=sllm,prompt = main_prompt, verbose=True)
-    temp = chain.predict(position=position, query=query)
+    chain = LLMChain(llm=sllm,prompt = main_prompt, verbose=True, memory=memory)
+    temp = chain.predict(position=position, human_input=human_input)
     print(temp)
+    print("-------------------------------------- ")
     return (temp)
